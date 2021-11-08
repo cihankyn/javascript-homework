71a245d8337a81b3031d467d5c40c4e6f9182453
Cihan Koyun <chnkyn01@gmail.com>
2021-11-09 02:54:25 +0300

    changed something in index.js

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 import { FirstDay } from "./day-1/program.js";
 const firstDay = new FirstDay();
 
-let first_day_answer_one = document.getElementById("first_day_answer_one");
-let first_day_answer_two = document.getElementById("first_day_answer_two");
-let first_day_answer_three = document.getElementById("first_day_answer_three");
-let first_day_answer_four = document.getElementById("first_day_answer_four");
-
-first_day_answer_one.innerText = firstDay.question_1();
-first_day_answer_two.innerText = firstDay.question_2();
-
 console.log(firstDay.question_1());
 console.log(firstDay.question_2());
 console.log(firstDay.question_3());
